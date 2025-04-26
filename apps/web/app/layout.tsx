6f576c566e9bf3e58f4e9f2b2f9cea3b01df44d9
John James <john.usa.james@gmail.com>
2025-04-26 15:06:43 +0100

    feat: add AppSidebar component and integrate into layout; update Providers to include SidebarProvider

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,8 @@ import {Geist, Geist_Mono} from "next/font/google"
 
 import "@workspace/ui/globals.css"
 import {Providers} from "@/components/providers"
+import {SidebarTrigger} from "@workspace/ui/components/sidebar"
+import {AppSidebar} from "@/components/app-sidebar"
 
 const fontSans = Geist({
   subsets: ["latin"],
@@ -21,7 +23,13 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}>
-        <Providers>{children}</Providers>
+        <Providers>
+          <AppSidebar />
+          <main className="p-4">
+            <SidebarTrigger />
+            {children}
+          </main>
+        </Providers>
       </body>
     </html>
   )
