8431ed19cdb50a595350143e35dad798e6b3d1c6
John James <john.usa.james@gmail.com>
2025-04-27 13:23:54 +0100

    feat: add schema types for category, page, site settings, and tag; update imports and turbo configuration

--- packages/cms-schema/src/index.ts
+++ packages/cms-schema/src/index.ts
@@ -1,6 +1,6 @@
-import {category} from "./category.js"
-import {page} from "./page.js"
-import {siteSettings} from "./site-settings.js"
-import {tag} from "./tag.js"
+import {category} from "./schema-types/category.js"
+import {page} from "./schema-types/page.js"
+import {siteSettings} from "./schema-types/site-settings.js"
+import {tag} from "./schema-types/tag.js"
 
 export const schemaTypes = [page, category, tag, siteSettings]
