321c6fbc73e0e4128c8b57f69068dcad6a8e8c07
John James <john.usa.james@gmail.com>
2025-05-07 01:10:14 +0100

    fix: remove production configuration from sanity config

--- packages/cms-schema/sanity.config.js
+++ packages/cms-schema/sanity.config.js
@@ -1,11 +1,9 @@
 import {defineConfig} from "sanity"
 import {schemaTypes} from "./dist/index.js"
-import {prodConfig} from "@workspace/sanity-config/prod"
 import {baseConfig} from "@workspace/sanity-config/base"
 
 export default defineConfig({
   ...baseConfig,
-  ...prodConfig,
   schema: {
     types: schemaTypes,
   },
